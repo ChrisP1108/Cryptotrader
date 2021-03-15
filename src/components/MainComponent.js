c2053ef4ca52c7b1ebbd00532f60485b6d43e1c0
Chris Paschall <ChrisP1108@gmail.com>
2021-03-15 15:16:43 -0700

    Initial Primary Components Setup

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,9 @@
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Software from './SoftwareComponent';
+import About from './AboutComponent';
+import ContactUs from './ContactUsComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,6 +11,13 @@ const Main = () => {
     return (
         <>
             <Header />
+            <Switch>
+                    <Route path='/home' component={Home} />
+                    <Route exact path='/software' component={Software} />
+                    <Route exact path='/about' component={About} />
+                    <Route exact path='/contactus' component={ContactUs} />
+                    <Redirect to='/home' />
+                </Switch>
             <Footer />
         </>
     )
