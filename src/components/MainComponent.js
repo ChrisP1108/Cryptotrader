341015e24cdf11622c62e2fa61446b9b6458aaaa
Chris Paschall <ChrisP1108@gmail.com>
2021-03-14 17:28:55 -0700

    Initial Setup Test

---
+++ src/components/MainComponent.js
@@ -0,0 +1,15 @@
+// import Header from './HeaderComponent';
+// import Footer from './FooterComponent';
+// import Home from './HomeComponent';
+// import About from './AboutComponent';
+// import Contact from './ContactComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const Main = () => {
+    return (
+        <h1>Cryptotrader Test</h1>
+    )
+}
+
+export default Main;
