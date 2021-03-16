38e0f9170b5b48203a0c027d58195e44ea831af2
Chris Paschall <ChrisP1108@gmail.com>
2021-03-15 22:03:18 -0700

    Initial Homepage Setup

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -33,13 +33,13 @@ const Header = () => {
                                 <NavLink activeClassName="navactive" className="nav-link navstyle" to="/home">HOME</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-3 ml-lg-5" to="/software">SOFTWARE</NavLink>
+                                <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/software">SOFTWARE</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-3 ml-lg-5" to="/about">ABOUT</NavLink>
+                                <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/about">ABOUT</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-3 ml-lg-5" to="/contactus">CONTACT US</NavLink>
+                                <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/contactus">CONTACT US</NavLink>
                             </NavItem>
                         </Nav>
                         <span className="navbar-text mx-auto d-flex justify-content-center">
