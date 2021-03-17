d22c016aae974e194e747455d4d756b6713cdcfb
Chris Paschall <ChrisP1108@gmail.com>
2021-03-17 16:23:31 -0700

    Contact Us Component Initial Completion

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,14 +6,17 @@ import About from './AboutComponent';
 import ContactUs from './ContactUsComponent';
 import { HOMEPAGECONTENTS } from '../shared/HomePageContents';
 import { SOFTWAREPAGECONTENTS } from '../shared/SoftwarePageContents';
+import { CONTACTPAGECONTENTS } from '../shared/ContactPageContents'
 import { useState } from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const Main = () => {
 
+    
     const [homePageContent, setHomePageContent] = useState(HOMEPAGECONTENTS);
     const [softwarePageContent, setSoftwarePageContent] = useState(SOFTWAREPAGECONTENTS);
+    const [contactUsPageContent, setContactUsPageContent] = useState(CONTACTPAGECONTENTS);
 
     return (
         <>
@@ -22,7 +25,7 @@ const Main = () => {
                     <Route path='/home' render={() => <Home homepagecontent={homePageContent} />} />
                     <Route path='/software' render={() => <Software softwarepagecontent={softwarePageContent[0]} />} />
                     <Route exact path='/about' component={About} />
-                    <Route exact path='/contactus' component={ContactUs} />
+                    <Route exact path='/contactus' render={() => <ContactUs contactuspagecontent={contactUsPageContent[0]} />} />
                     <Redirect to='/home' />
                 </Switch>
             <Footer />
