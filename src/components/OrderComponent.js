16f333e2603864008a6b2c402bc5c6926b9d0ebb
Chris Paschall <ChrisP1108@gmail.com>
2021-03-23 22:36:59 -0700

    Order Page Add To Cart Initial Completion

--- src/components/OrderComponent.js
+++ src/components/OrderComponent.js
@@ -1,16 +1,51 @@
-import React from 'react'
+import { useState } from 'react';
 
-const Order = () => {
+const Order = ({ orderpagecontent }) => {
+
+    const [cartItems, setCartItems] = useState([]);
+    const [cartTotal, setCartTotal] = useState(0);
+
+    const addItem = (item) => {
+        if (cartItems.includes(item)) {
+            return;
+        }
+        setCartTotal(cartTotal + item.price);
+        setCartItems([].concat(cartItems, item))
+        console.log(cartItems);
+    }
     
-    const OrderList = () => {
+    const orderlist = orderpagecontent.map(list => {
         return (
-            <h5>Product List</h5>
+            <div key={list.id} className="list-overlay" onClick={() => addItem(list)}>
+                <h5>{list.title}</h5>
+            </div>
+        )
+    });
+
+    const cartlist = cartItems.map(cartitem => {
+        return (
+            <div key={cartitem.id} className="mt-5">
+                <h5>{cartitem.title}</h5>
+            </div>
+        )
+    })
+
+    const OrderTotal = () => {
+        return(
+            <h4>Price Total: ${cartTotal}</h4>
         )
     }
     
     const OrderSelected = () => {
         return (
-            <h5>Product Selected</h5>
+            <>
+                <div>
+                    <h5>{cartlist}</h5>
+                </div>
+                <div className="mt-5">
+                    <OrderTotal />
+                </div>
+            </>
         )
     }
 
@@ -24,16 +59,16 @@ const Order = () => {
                         </div>
                     </div>
                     <div className="row mt-5">
-                        <div className="col-lg-4 order-overlay my-auto mx-auto pt-2">
-                            <h2>Click Below To Add To Cart</h2>
+                        <div className="col-lg-4 order-overlay mx-auto pt-2">
+                            <h2>Click Items Below To Add To Cart</h2>
                             <div className="mt-5">
-                                <OrderList />
+                                {orderlist}
                             </div>
                         </div>
-                        <div className="col-lg-4 order-overlay my-auto mx-auto mt-3 mt-lg-0 pt-2">
+                        <div className="col-lg-4 order-overlay mx-auto mt-3 mt-lg-0 pt-2">
                             <h2>Product Cart</h2>
                             <div className="mt-5">
-                                <OrderSelected />
+                                {cartItems === '' ? <h5>No Items Selected</h5> : <OrderSelected />}
                             </div>
                         </div>
                     </div>
